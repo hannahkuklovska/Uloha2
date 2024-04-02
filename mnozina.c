7b481f849f485ae7c147ddb8fb20994fbdc5e78d
hannahkuklovska <hannah15072002@gmail.com>
2024-04-02 15:54:05 +0200

    tlac mnoziny

---
+++ mnozina.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+
+void tlac_mnoziny(int mnozina[N])
+{
+     for (int i = 0; i < N; i++)
+     {
+          printf("%d ", mnozina[i]);
+     }
+}
+
+void main()
+{
+     int mnozina_1[N], int mnozina_2[L];
+     int mnozina[3] = {1, 2, 3};
+     int mnozina[2] = {2, 4};
+
+     for (int i = 0; i < N; i++)
+     {
+          for (int j = 0; i < L; i++)
+          {
+               if (mnozina_1[i] == mnozina_2[j])
+               {
+               }
+          }
+     }
+}
