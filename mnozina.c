a6605216298faaab6e9a3c0c80a4300fbb13a0cd
hannahkuklovska <hannah15072002@gmail.com>
2024-04-02 16:41:24 +0200

    priradenie všetkých prvkov množiny 1 do množiny 3, potom všetky prvky množiny 2 do množiny 3, ešte vymyslieť podmienku, keď sa prvky opakujú

--- mnozina.c
+++ mnozina.c
@@ -3,15 +3,17 @@
 
 void tlac_mnoziny(int mnozina[], int N)
 {
+
      for (int i = 0; i < N; i++)
      {
-          printf("%d ", mnozina[i]);
+          printf("%6d", mnozina[i]);
      }
      printf("\n");
 }
 
 int prienik_mnozin(int mnozina_1[], int mnozina_2[], int N, int L)
 {
+
      int K;
      int mnozina_3[K];
      for (int i = 0; i < N; i++)
@@ -28,12 +30,32 @@ int prienik_mnozin(int mnozina_1[], int mnozina_2[], int N, int L)
      tlac_mnoziny(mnozina_3, K);
 }
 
+int zjednotenie_mnozin(int mnozina_1[], int mnozina_2[], int N, int L)
+{
+
+     int K;
+     int mnozina_3[K];
+     for (int i = 0; i < N; i++)
+     {
+          for (int j = 0; i < L; i++)
+          {
+               {
+
+                    mnozina_3[K++] = mnozina_1[i];
+                    mnozina_3[K++] = mnozina_2[j];
+               }
+          }
+     }
+     tlac_mnoziny(mnozina_3, K);
+}
+
 void main()
 {
 
      int mnozina_1[3] = {1, 2, 3};
-     int mnozina_2[2] = {2, 4};
+     int mnozina_2[2] = {2, 5};
      int velkost_prieniku;
      tlac_mnoziny(mnozina_1, 3);
      prienik_mnozin(mnozina_1, mnozina_2, 3, 2);
+     zjednotenie_mnozin(mnozina_1, mnozina_2, 3, 2);
 }
