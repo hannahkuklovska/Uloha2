3785cd92e60b978c41a0ae3d98ca6d61eb8c4c7a
hannahkuklovska <hannah15072002@gmail.com>
2024-04-10 18:06:59 +0200

    návratován hodnota prieniku množín = MNOZINA

--- mnozina.c
+++ mnozina.c
@@ -17,26 +17,23 @@ void tlac_mnoziny(MNOZINA mnozina)
      printf("\n");
 }
 
-int prienik_mnozin(int *mnozina_1, int *mnozina_2, int N, int L, int *prien_velk)
+MNOZINA prienik_mnozin(MNOZINA mnozina_1, MNOZINA mnozina_2)
 {
 
-     int K = 0;
-     int mnozina_3[N];
-     for (int i = 0; i < N; i++)
+     MNOZINA vysledok = {0};
+     for (int i = 0; i < mnozina_1.N; i++)
      {
-          for (int j = 0; j < L; j++)
+          for (int j = 0; j < mnozina_2.N; j++)
           {
-               if (mnozina_1[i] == mnozina_2[j])
+               if (mnozina_1.p[i] == mnozina_2.p[j])
                {
+                    vysledok.p[vysledok.N++] = mnozina_1.p[i];
 
-                    mnozina_3[K] = mnozina_1[i];
-                    K++;
                     break;
                }
           }
      }
-     *prien_velk = K;
-     tlac_mnoziny(mnozina_3, K);
+     return vysledok;
 }
 
 int zjednotenie_mnozin(int *mnozina_1, int *mnozina_2, int N, int L)
