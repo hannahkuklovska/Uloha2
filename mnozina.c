4eafb92e9905828f3a6fcbddd8c5c987526f65f0
hannahkuklovska <hannah15072002@gmail.com>
2024-04-16 21:42:48 +0200

    zväčšenie veľkosti množiny o 1 pri každej iterácií

--- mnozina.c
+++ mnozina.c
@@ -75,6 +75,7 @@ MNOZINA generator_mnoziny(int velkost, int dh, int hh)
      for (int i = 0; i < velkost; i++)
      {
           mnozina.p[i] = dh + rand() % (hh - dh + 1);
+          mnozina.N++;
      }
 
      return mnozina;
